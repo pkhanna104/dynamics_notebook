ceadfc8f7898be683da0d1c885580f203a8253ef
preeya <pkhanna104@gmail.com>
2020-04-24 12:05:20 -0700

    adding files developed in google colab

---
+++ lds_utils.py
@@ -0,0 +1,296 @@
+#'3.6.9 (default, Nov  7 2019, 10:44:02) \n[GCC 8.3.0]'
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+
+def plot_example_x0(A, nEx = 10, nT = 100, x1lim = (-5, 5), x2lim = (-5, 5), title = ''):
+    ''' 
+    function to take a given A matrix, randomly generate initial condition points in 
+        2D space, and plot the resultant 2D trajectories when initial condition is propagated by A
+
+    inputs: 
+        A -- 2 x 2 dynamics matrix 
+        nEx -- number of examples per x/y axis. Total number of examples = nEx**2
+        nT -- number of time steps to propogate;  
+    ''' 
+    try:
+        assert(A.shape[0] == A.shape[1] == 2)
+    except:
+        raise Exception('A must be a 2 x 2 matrix, this is a %d x %d matrix '%(A.shape[0], A.shape[1]))
+
+    ### Generate nEx^2 examples of initial conditions, and propogate them nT steps; 
+    ### Initial conditions grid + some noise to make visualization easier; 
+    assert(x1lim[1] > x1lim[0])
+    assert(x2lim[1] > x2lim[0])
+    
+    x1_init = np.linspace(x1lim[0], x1lim[1], nEx) + 0.1*np.random.randn(nEx)
+    x2_init = np.linspace(x2lim[0], x2lim[1], nEx) + 0.1*np.random.randn(nEx)
+
+    ### Setup the figure; 
+    f, ax = plt.subplots(figsize = (5, 5))
+
+    for i, xi1 in enumerate(x1_init):
+        for j,xi2 in enumerate(x2_init):
+            x0 = np.mat([[xi1], [xi2]])
+
+            ### Save the trajectory ###
+            x_traj = [x0]
+
+            ### Generate the trajectory ###
+            for it in range(nT):
+                xi = np.dot(A, x0)
+                x_traj.append(xi)
+                x0 = xi.copy()
+            
+            ### Plot the trajectory 
+            x_traj = np.hstack((x_traj)).T 
+            assert(x_traj.shape[1] == 2)
+            assert(x_traj.shape[0] == nT + 1)
+
+            cax = ax.scatter(m2a(x_traj[:, 0]), m2a(x_traj[:, 1]), s=None, c=np.arange(nT+1), cmap = 'viridis')
+    cbar = f.colorbar(cax)
+    cbar.set_label('Timesteps', rotation = 270)
+    ax.set_xlabel('$x^0$', fontsize=14)
+    ax.set_ylabel('$x^1$', fontsize=14)
+    ax.set_title(title, fontsize=14)
+
+def eigenspec(*args, labels=None, dt = 0.01, xlim = None, ylim = None, axi = None, skip_legend=False):
+    ''' 
+    Method to plot the time decay (seconds) vs. frequency (hz) of eigenvalues of A for 
+    each A in args
+    '''
+
+    if labels is not None:
+        assert(len(labels) == len(args))
+    
+    ### Setup distinct colors for each A matrix
+    N = len(args)
+    colors = pl.cm.viridis(np.linspace(0,1,N))
+
+    ### Setup plot
+    if axi is None:
+      f, ax = plt.subplots()
+    else:
+      ax = axi
+      skip_legend = True
+
+    for ia, A in enumerate(args):
+
+        ### Make sure square matrix 
+        assert(A.shape[0] == A.shape[1])
+
+        ### Get eigenvalues: 
+        ev, _ = np.linalg.eig(A)
+
+        ### Get R / theta values
+        R = np.abs(ev)
+        Th = np.angle(ev)
+
+        ## Get time decay
+        TD = -1/np.log(R)*dt 
+
+        ## Get Hz: 
+        Hz = Th / (2*np.pi*dt)
+
+        if labels is None:
+            lab = 'A%d'%(ia+1)
+        else:
+            lab = labels[ia]
+        ax.vlines(Hz, 0, TD, color=colors[ia])
+        ax.plot(Hz, TD, '.', color=colors[ia], label=lab, markersize=20)
+    
+    if skip_legend:
+        pass
+    else:
+        f.legend()
+        
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
+
+    ax.set_ylabel('Time Decay (sec)')
+    ax.set_xlabel('Frequency (Hz)')
+
+###### PLOTTING TOP DIM #####
+def flow_field_plot_top_dim(A, X, dt, dim0 = 0, dim1 = 1, cmax = .1,
+    scale = 1.0, width = .04):
+    ''' 
+    method to plot flow field plus first 100 data points in X after transforming 
+    to the eigenvector basis 
+
+    cmax, scale, and width and direct inputs to the plot_flow fcn
+    '''
+
+    assert(A.shape[0] == A.shape[1])
+    assert(A.shape[0] == X.shape[1])
+    nT, nD = X.shape
+
+    ### Get the eigenvalue / eigenvectors: 
+    T, evs = get_sorted_realized_evs(A)
+    T_inv = np.linalg.pinv(T)
+
+    ### Linear transform of A matrix
+    Za = np.real(np.dot(T_inv, np.dot(A, T)))
+    
+    ### Transfrom the data; 
+    Z = np.dot(T_inv, X.T).T
+
+    xmax = np.max(np.real(Z[:100, dim0]))
+    xmin = np.min(np.real(Z[:100, dim0]))
+    ymax = np.max(np.real(Z[:100, dim1]))
+    ymin = np.min(np.real(Z[:100, dim1]))
+
+    ### Which eigenvalues are these adn what are their properties? 
+    td = -1/np.log(np.abs(evs[[dim0, dim1]]))*dt; 
+    hz0 = np.angle(evs[dim0])/(2*np.pi*dt)
+    hz1 = np.angle(evs[dim1])/(2*np.pi*dt)
+
+    ### Now plot flow field in top lambda dimensions
+    f, ax = plt.subplots()
+    ax.axis('equal')
+    plot_flow(Za, ax, dim0=dim0, dim1=dim1, xmax=xmax, xmin=xmin, ymax=ymax, 
+      ymin=ymin, cmax = cmax, scale = scale, width = width)
+
+    ax.plot(Z[:100, dim0], Z[:100, dim1], 'k.-', alpha=.5)
+    ax.set_xlabel('$z_{%d}$'%dim0, fontsize=14)
+    ax.set_ylabel('$z_{%d}$'%dim1, fontsize=14)
+    
+    title_str = '$\lambda_{%d}$ Time Decay =%.2f sec, Hz = %.2f,\n $\lambda_{%d}$ Time Decay=%.2f sec, Hz = %.2f'%(dim0, td[0], hz0, dim1, td[1], hz1)
+    ax.set_title(title_str, fontsize=14)
+
+def get_sorted_realized_evs(A):
+    '''
+    This method gives us the sorted eigenvalues/vectors that yields top dynamical dimensions
+    as the first dimensions. For complex eigenvalues it also sets the first complex conjugate equal 
+    to the real part and the second equal to the imaginary part. See https://www.youtube.com/watch?v=qlUr2Jc5O0g
+    for more details; 
+    ''' 
+
+    ## Make sure A is a square; 
+    assert(A.shape[0] == A.shape[1])
+    
+    ### Get eigenvalues / eigenvectors: 
+    ev, evects = np.linalg.eig(A)
+
+    ### Doesn't always give eigenvalues in order so sort them here: 
+    ix_order = np.argsort(np.abs(ev)) ## This sorts them in increasing order; 
+    ix_order_decreasing = ix_order[::-1]
+
+    ### Sorted in decreasing order
+    ev_sort = ev[ix_order_decreasing]
+    evects_sort = evects[:, ix_order_decreasing]
+
+    ### Make sure these eigenvectors/values still abide by Av = lv:
+    chk_ev_vect(ev_sort, evects_sort, A)
+
+    ### Now for imaginary eigenvalue, set the first part equal to the real, 
+    ## and second part equal to the imaginary: 
+    nD = A.shape[0]
+
+    # Skip indices if complex conjugate
+    skip_ix = [] 
+
+    ## Go through each eigenvalue
+    for i in range(nD):
+        if i not in skip_ix:
+            if np.imag(ev_sort[i]) != 0:
+                evects_sort[:, i] = np.real(evects_sort[:, i])
+
+                assert(np.real(ev_sort[i+1]) == np.real(ev_sort[i]))
+                assert(np.imag(ev_sort[i+1]) == -1*np.imag(ev_sort[i]))
+
+                evects_sort[:, i+1] = np.imag(evects_sort[:, i+1])
+                skip_ix.append(i+1)
+
+    return evects_sort, ev_sort
+    
+
+####### UTILS ######
+def plot_flow(A, axi, nb_points=20, xmin=-5, xmax=5, ymin=-5, ymax=5, dim0 = 0, dim1 = 1,
+    scale = .5, alpha=1.0, width=.005, cmax=.1):
+
+    ''' Method to plot flow fields in 2D 
+        Inputs: 
+            A : an nxn matrix (datatype should be array)
+            axi: axis on which to plot 
+            nb_points: number of arrows to plot on x-axis adn y axis 
+            x/y, min/max: limits of flow field plot 
+            dim0: which dimension of A to plot on X axis; 
+            dim1: which dimension of A to plot on Y axis; 
+    '''
+
+    x = np.linspace(xmin, xmax, nb_points)
+    y = np.linspace(ymin, ymax, nb_points)
+    # create a grid
+    X1 , Y1  = np.meshgrid(x, y)                       
+    
+    ### For each position on the grid, (x1, y1), use A to compute where the next 
+    ### point would be if propogate (x1, y1) by A -- assuming all other dimensions are zeros
+    DX, DY = compute_dX(X1, Y1, A, dim0, dim1)  
+
+    ### Get magnitude of difference
+    M = (np.hypot(DX, DY))         
+
+    ### Use quiver plot -- NOTE: axes must be "equal" to see arrows properly. 
+    Q = axi.quiver(X1, Y1, DX, DY, M, units = 'xy', scale = scale,
+        pivot='mid', cmap=plt.cm.viridis, width=width, alpha=alpha,
+        clim = [0., cmax])
+
+def compute_dX(X, Y, A, dim0, dim1):
+    '''
+    method to compute dX based on A
+    '''
+
+    newX = np.zeros_like(X)
+    newY = np.zeros_like(Y)
+
+    nrows, ncols = X.shape
+
+    for nr in range(nrows):
+        for nc in range(ncols):
+
+            ### Assume everything is zero except dim1, dim2; 
+            st = np.zeros((len(A), 1))
+            st[dim0] = X[nr, nc]; 
+            st[dim1] = Y[nr, nc];
+
+            st_nx = np.dot(A, st)
+            newX[nr, nc] = st_nx[dim0]
+            newY[nr, nc] = st_nx[dim1]
+
+    ### Now to get the change, do new - old: 
+    DX = newX - X; 
+    DY = newY - Y; 
+
+    return DX, DY
+
+def chk_ev_vect(ev, evect, A):
+    '''
+    Check that each eigenvlaue / vecotr is correct: 
+    '''
+    for i in range(len(ev)):
+        evi = ev[i]
+        vct = evect[:, i]
+        ## Do out the multiplication
+        assert(np.allclose(np.dot(A, vct[:, np.newaxis]), evi*vct[:, np.newaxis]))
+
+def m2a(x):
+  '''
+  method to squeeze 1D matrix into array format for easier plotting
+  '''
+  return np.squeeze(np.array(x))
+
+def get_population_R2(X, X_est):
+    ''' 
+    get the variance in X accounted for in X_est 
+    '''
+    assert(X.shape == X_est.shape)
+
+    ### Sum squared of residuals
+    SSR = np.sum((X - X_est)**2)
+
+    ### Sum squared total: 
+    SST = np.sum((X - np.mean(X.reshape(-1)))**2)
+
+    return 1 - SSR/SST
---
+++ seq_utils.py
@@ -0,0 +1,209 @@
+import numpy as np 
+import scipy
+import scipy.signal 
+from scipy.signal import butter, filtfilt, hilbert, sosfiltfilt
+
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KernelDensity
+from sklearn.model_selection import GridSearchCV
+
+def generate_seq(f_fund, std_gauss, frac_offset, total_length=None, N_neurons=None, dt=0.01,
+  pad_zeros = 0.0, end_pad = 0., beg_pad = 0.):
+  ''' 
+  method to generate a sequence of neural activity composed of individual 
+  neuron fluctuations at f_fund (frequency), convolved with a gaussian (std_gauss),
+  with each neuron at an offset with respect to one another (dictated by frac_offset). And 
+  enough neurons are included to make the total length of the sequence equal to total_length. 
+
+  Input units: 
+    f_fund: frequency (Hz)
+    std_gauss: standard deviation of gaussian (sec)
+    frac_offset: fraction of f_fund cycle
+    total_lenth: seconds OR
+    N_neurons: integer unit
+    dt: seconds
+  '''
+  ### Get the number of indices in the std_gauss ###
+  std_ind = int(np.ceil(std_gauss/dt))
+
+  ### Make the gaussian envelope ###
+  gaussian_env = scipy.signal.gaussian(2*std_ind, std_ind, sym=True)
+
+  ### Make the sine wave, centered at the middle of the gaussian; 
+  sine_t = np.linspace(-std_gauss, std_gauss, len(gaussian_env))
+  sine = np.cos(2*np.pi*f_fund*sine_t)
+  
+  ### Get unit length ###
+  unitL = len(sine)
+  unitL_half = int(0.5*unitL)
+
+  ## N_units
+  if total_length is None:
+    N_units = N_neurons
+  else:
+    ### Now use this to tile 
+    ind_offs = ( frac_offset / f_fund ) / dt # frac cycles to seconds to timesteps 
+    N_units = int(np.floor((total_length / dt ) / ind_offs))
+  
+  ### Multiple to get a burst unit: 
+  unit_mod = sine*gaussian_env 
+  ### Total indices length / offsets --> how many offsets do we need?
+  #N_units = int(np.floor((total_length / dt) / ind_offs))
+
+  ### Now use this to tile 
+  ind_offs = ( frac_offset / f_fund ) / dt # frac cycles to seconds to timesteps 
+  
+  ### Total length of time; 
+  ind_tot = int(unitL_half + N_units*ind_offs + unitL_half)
+
+  ### Initialize the data: 
+  Data = np.zeros((ind_tot, N_units))
+
+  for i_n in range(N_units):
+    ix_start = int(unitL_half + i_n*ind_offs)
+    Data[ix_start - unitL_half:ix_start + unitL_half, i_n] = unit_mod.copy()
+  
+  if pad_zeros > 0:
+    pz = int(pad_zeros/dt)
+    pad = np.zeros((pz, N_units))
+    Data = np.vstack((pad, Data, pad))
+  if end_pad > 0:
+    pz = int(end_pad/dt)
+    pad = np.zeros((pz, N_units))
+    Data = np.vstack((Data, pad))
+  if beg_pad > 0:
+    pz = int(beg_pad/dt)
+    pad = np.zeros((pz, N_units))
+    Data = np.vstack((pad, Data))
+  return Data
+
+
+def fit_density(frequencies):
+
+  bandwidths = 10 ** np.linspace(-1, 1, 100)
+  grid = GridSearchCV(KernelDensity(kernel='gaussian'),
+                    {'bandwidth': bandwidths})
+  grid.fit(frequencies[:, None]);
+  #Now we can find the choice of bandwidth which maximizes the score 
+  #(which in this case defaults to the log-likelihood):
+
+  bw = grid.best_params_['bandwidth']
+
+  kde = KernelDensity(bandwidth=bw, kernel='gaussian')
+  kde.fit(frequencies[:, None])
+
+  # score_samples returns the log of the probability density
+  x_f = np.linspace(-10, 10, 10000)
+  logprob = kde.score_samples(x_f[:, None])
+
+  return x_f, np.exp(logprob)
+
+def sweep_fund_freq_and_offset(fund_freqs, dt_offset, N_neurons, gauss_std = 0.5, dt = 0.01,
+                               noise_level = 0.1, pad_zeros = 0.5, nreps = 20):
+    '''
+    method to interate through diff frequencies/frac_offsets of underlying single
+    unit modulation
+    
+    plots the non-negative frequency eigenvalues in a power spectra-like plot
+    '''
+
+    ### Data -- sequence example ###
+    f_dat, ax_dat = plt.subplots(figsize=(15, 9), ncols = len(fund_freqs), nrows = len(dt_offset))
+    
+    ### Results -- eigenspec ###
+    f, ax = plt.subplots(figsize=(15, 9), ncols = len(fund_freqs), nrows = len(dt_offset))
+    
+    for i_ff, freq in enumerate(fund_freqs):
+
+      for i_t, dt_off in enumerate(dt_offset):
+        
+        ### Generate sequence data ##
+        frac = dt_off*freq
+        data = generate_seq(freq, gauss_std, frac, N_neurons=N_neurons, dt = dt,
+                                      pad_zeros = pad_zeros)
+        nT, nD = data.shape
+        
+        ### Trial data used later ###
+        R2 = []; EVs = []; 
+        for ntrl in range(nreps):
+            trl_data = data + noise_level*np.random.randn(nT, nD)
+            data_trl_t = trl_data[1:, :].copy()
+            data_trl_tm1 = trl_data[:-1, :].copy()
+
+            ### Estimate A ###
+            ### Compute the A matrix ###
+            Aest = np.linalg.lstsq(data_trl_tm1, data_trl_t, rcond=None)[0] 
+            Aest = Aest.T
+            
+            ### Get non-negative angle EVs; 
+            ev, _ = np.linalg.eig(Aest)
+            an = np.angle(ev)
+            EVs.append(ev)
+
+            ##### Test Data ####
+            trl_data2 = data + noise_level*np.random.randn(nT, nD)
+            data_trl_t2 = trl_data2[1:, :].copy()
+            data_trl_tm12 = trl_data2[:-1, :].copy()
+            R2.append(lds_utils.get_population_R2(data_trl_t2, np.dot(Aest, data_trl_tm12.T).T))
+        
+            ### Eigenvalues; 
+            lds_utils.eigenspec(Aest, axi=ax[i_t, i_ff], xlim=(-.5, 10.), ylim = (0., 0.8), skip_legend = True)
+        
+        ### Plot noisy data
+        ax_dat[i_t, i_ff].pcolormesh(np.arange(nT+1)*dt, np.arange(nD+1), trl_data.T, cmap='binary', vmin=-1, vmax=1)
+        
+        ### Plot distribution of eigenvalue frequencies ###
+        EVs = np.hstack((EVs))
+        fr = np.angle(EVs)/(2*np.pi*dt)
+        x_f, freq_density = seq_utils.fit_density(fr)
+        ax2 = ax[i_t, i_ff].twinx()
+        ax2.plot(x_f, freq_density, 'r--')
+
+        ## Find peaks: 
+        peaks, _ = scipy.signal.find_peaks(freq_density, width = 100, prominence=.02)
+        ax2.plot(x_f[peaks], freq_density[peaks], 'r*')
+        
+        freq_dens_nneg = freq_density[x_f >= 0]
+        x_f_nneg = x_f[x_f>= 0]
+        ix_max = np.argmax(freq_dens_nneg)
+        ax2.plot(x_f_nneg[ix_max], freq_dens_nneg[ix_max], 'r*')
+        ax2.set_xlim([-.5, 10])
+        ax2.set_ylim([0., 1.])
+        
+        ### Label axes 
+        if i_ff == 0:
+          ax[i_t, i_ff].set_ylabel('Offs %.2f sec'%dt_off)
+          ax_dat[i_t, i_ff].set_ylabel('Offs %.2f sec'%dt_off)
+        
+        if i_t == len(dt_offset)-1:
+          ax[i_t, i_ff].set_xlabel('Freq %.1f Hz'%freq)
+          ax_dat[i_t, i_ff].set_xlabel('Freq %.1f Hz'%freq)
+
+        R2 = np.hstack((R2))
+        ax[i_t, i_ff].set_title('R2 = %.2f +/- %.2f' %(np.mean(R2), np.std(R2)))
+    f.tight_layout()
+    f_dat.tight_layout()
+
+def simple_fft(signal, Fs):
+    # Number of samplepoints
+    N = len(signal)
+    # sample spacing
+    T = 1.0 / Fs
+    yf = scipy.fftpack.fft(signal)
+    xf = np.linspace(0.0, 1.0/(2.0*T), int(N/2))
+    yf_sub = 2.0/N * np.abs(yf[:N//2])
+    return xf, yf_sub
+
+### Smoothed spikes ###
+def get_smoothed_spks(spks, binsize, smoothsize):
+    ### Assumes a time x neuron x trl format
+    SPKS_smooth = np.zeros_like(spks)
+    std = smoothsize/binsize
+    window = scipy.signal.gaussian(21., std)
+    window = window / float(np.sum(window))
+    smooth = [];
+    for trl in range(SPKS_smooth.shape[2]):
+        for n in range(SPKS_smooth.shape[1]):
+            SPKS_smooth[:, n, trl] = np.convolve(window, spks[:, n, trl], mode='same')
+    return SPKS_smooth
